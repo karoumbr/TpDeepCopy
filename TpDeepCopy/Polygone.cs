bbc5b696af4ba9ac16c0927388835d27d585cd1d
Karim Ben Romdhane <karoumbr@gmail.com>
2023-09-17 16:48:28 +0100

    initial commit

---
+++ TpDeepCopy/Polygone.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TpDeepCopy
+{
+    class Polygone
+    {
+        public Point[] points;
+
+        public Polygone(Point[] points)
+        {
+            this.points = points;
+        }
+    }
+}
---
+++ TpDeepCopy/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TpDeepCopy
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+           // Décomentez pour tester
+
+           Point p1 = new Point(1,2);
+           Point p2 = new Point(p1); // deep copy
+
+           p2.x = 3;
+           Debug.Assert(p1.x != p2.x, "Problème sur le constructeur par copie de Point.");
+
+           Point[] points = {p1, p2};
+
+           Polygone po1 = new Polygone(points);
+           Polygone po2 = new Polygone(po1); // deep(er) copy
+
+           po2.points[0].x = 4;
+           Debug.Assert(po2.points[0].x != po1.points[0].x, "Problème sur le constructeur par copie de Polygone.");
+           */
+        }
+    }
+}
